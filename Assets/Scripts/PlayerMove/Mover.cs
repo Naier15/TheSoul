ccd261598b600b3964df99f4f55a8ce45587a21f
Maxsman <dekxirus@gmail.com>
2022-01-28 20:44:10 +1000

    Add move logic without phys

--- Assets/Scripts/PlayerMove/Mover.cs
+++ Assets/Scripts/PlayerMove/Mover.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 namespace PlayerMove
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Transform))]
     public class Mover : MonoBehaviour
     {
         [SerializeField] private float _speed;
 
-        private Rigidbody _rigidbody;
-
+        private Transform _transform;
+        
         private void Start()
         {
-            _rigidbody = GetComponent<Rigidbody>();
+            _transform = GetComponent<Transform>();
         }
 
         public void Move()
         {
             Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            _rigidbody.AddForce(movement * _speed);
+            transform.Translate(movement * _speed * Time.fixedDeltaTime);
         }
     }
 }
