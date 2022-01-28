16d5d4c817be7fadc3da80b16a33f751e3b3e07b
Roman <grv1510@mail.ru>
2022-01-29 08:27:56 +1000

    added field, torch and set portals at right places

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -6,13 +6,13 @@ public class Maze : MonoBehaviour
 {
     private Transform _transform;
 
-    private void Start()
+    private void Awake()
     {
         _transform = GetComponent<Transform>();
     }
 
     public void Flip()
     {
-        _transform.Rotate(180, 0, 0);
+        _transform.Rotate(0, 0, 180);
     }
 }
